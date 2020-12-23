ab339baed5e92875c6b2c1264d9c67a74a054dce
guncci <guncci0403@gmail.com>
2020-12-23 10:18:59 +0900

    1.jsp 프로젝트 환경구성 2.20201222 수업 내용 정리

---
+++ src/main/java/jsp/Test.java
@@ -0,0 +1,36 @@
+package jsp;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+/**
+ * Test.java
+ *
+ * @author PC-13
+ * @version 1.0
+ * @see
+ *
+ * <pre>
+ * << 개정이력(Modification Information) >>
+ *
+ * 수정자 수정내용
+ * ------ ------------------------
+ * PC-13 최초 생성
+ *
+ * </pre>
+ */
+public class Test {
+
+	/**
+	 * Method : main
+	 * 작성자 : PC-13
+	 * 변경이력 :
+	 * @param args
+	 * Method 설명 :
+	 */
+	public static void main(String[] args) {
+		private static final Logger logger = LoggerFactory.getLogger(Test.class);
+
+
+	}
+}
